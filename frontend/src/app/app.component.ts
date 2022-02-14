cd5d35d0abc2f778e77653dc490f74083532e331
Naveed Baloch <naveed@badriconsultancy.com>
2022-02-14 17:48:46 +0500

    feat: frontend and backend connection via proxy

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frontend';
+  constructor(private http: HttpClient) {
+    this.http.get('/api').subscribe(console.log);
+  }
 }
